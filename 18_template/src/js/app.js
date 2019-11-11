166ffeca93df9fbbdc5c8c99c23084565160741a
tartalon <tartalon23@gmail.com>
2019-11-12 01:12:39 +0200

    svg

--- 18_template/src/js/app.js
+++ 18_template/src/js/app.js
@@ -1,3 +1,7 @@
-document.querySelector('.header__menu-burger').addEventListener('click', function() {
+document.querySelector('.burger__icon').addEventListener('click', function() {
 	document.querySelector('.header__menu-wrapper').classList.toggle('element');
 })
+
+// document.querySelector('.burger__close').addEventListener('click', function() {
+// 	document.querySelector('.header__menu-wrapper').classList.toggle('close');
+// })
