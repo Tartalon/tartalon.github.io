29d0aa9f7b305f1f5d3f6dea3933ecb1fde1afdf
Vladimir <tartalon23@gmail.com>
2019-11-12 17:23:12 +0200

    javascript menu

--- 18_template/src/js/app.js
+++ 18_template/src/js/app.js
@@ -1,7 +1,13 @@
-document.querySelector('.burger__icon').addEventListener('click', function() {
-	document.querySelector('.header__menu-wrapper').classList.toggle('element');
-})
 
-// document.querySelector('.burger__close').addEventListener('click', function() {
-// 	document.querySelector('.header__menu-wrapper').classList.toggle('close');
-// })
+	const open = document.querySelector('.burger__icon');
+	const menu = document.querySelector('header__menu-wrepper');
+
+	open.addEventListener ('click', 
+	function () {
+		menu.style.display='block';
+	});
+
+
+// document.querySelector('.burger__icon').addEventListener('click', function() {
+// 	document.querySelector('.header__menu-wrapper').style.display = 'block';
+// });
