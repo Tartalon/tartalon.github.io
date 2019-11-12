9fdcba8d1307b47a1d7dac8ae97c0acf02eb74f8
tartalon <tartalon23@gmail.com>
2019-11-13 00:48:41 +0200

    cookie

--- 18_template/src/js/app.js
+++ 18_template/src/js/app.js
@@ -1,13 +1,7 @@
 
-	const open = document.querySelector('.burger__icon');
-	const menu = document.querySelector('header__menu-wrepper');
-
-	open.addEventListener ('click', 
-	function () {
-		menu.style.display='block';
-	});
-
-
-// document.querySelector('.burger__icon').addEventListener('click', function() {
-// 	document.querySelector('.header__menu-wrapper').style.display = 'block';
-// });
+document.querySelector('.burger__icon').addEventListener('click', function () {
+  document.querySelector('.header__menu-wrapper').style.display = 'block';
+});
+document.querySelector('.burger__close').addEventListener('click', function () {
+  document.querySelector('.header__menu-wrapper').style.display = 'none';
+});
