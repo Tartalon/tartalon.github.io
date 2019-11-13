c1e736a3fcc09eac318b45cc01f47f6dfb6209d6
Vladimir <tartalon23@gmail.com>
2019-11-13 17:36:00 +0200

    form

--- 18_template/src/js/app.js
+++ 18_template/src/js/app.js
@@ -5,3 +5,7 @@ document.querySelector('.burger__icon').addEventListener('click', function () {
 document.querySelector('.burger__close').addEventListener('click', function () {
   document.querySelector('.header__menu-wrapper').style.display = 'none';
 });
+
+document.querySelector('.cookie__btn').addEventListener('click', function () {
+  document.querySelector('.header__cookie').style.display='none';
+})
