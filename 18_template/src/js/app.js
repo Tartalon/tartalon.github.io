cfb3534482793a78fb6334afb73d5d5c9d8d3645
tartalon <tartalon23@gmail.com>
2019-11-14 00:50:32 +0200

    form js

--- 18_template/src/js/app.js
+++ 18_template/src/js/app.js
@@ -9,3 +9,11 @@ document.querySelector('.burger__close').addEventListener('click', function () {
 document.querySelector('.cookie__btn').addEventListener('click', function () {
   document.querySelector('.header__cookie').style.display='none';
 })
+
+document.querySelector('.header__btn-contact').addEventListener('click', function () {
+  document.querySelector('.form__wrapper').style.display = 'block';
+})
+
+document.querySelector('.close-modal').addEventListener('click', function () {
+  document.querySelector('.form__wrapper').style.display='none';
+})
