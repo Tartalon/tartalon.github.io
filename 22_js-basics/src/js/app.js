8c06d5a781f426a8df087140d9b28c0bba2a91df
tartalon <tartalon23@gmail.com>
2019-11-26 17:20:36 +0200

    js

---
+++ 22_js-basics/src/js/app.js
@@ -0,0 +1,28 @@
+// const name = prompt ("Waht is your name?");
+// alert ("hello" + " " + name);
+
+// const year = 2019;
+// let birthday = prompt ("year of your birth?");
+// alert ("you are" + " " + (year - birthday) + " " + "years old");
+
+// let lenght = prompt ("what is the length of the side of the square?");
+// const perimetr = (4 * lenght);
+// alert ("perimetr of the square is" + " " + perimetr);
+
+// let radius = prompt ("what radius?");
+// const area = ((radius * radius) * Math.PI);
+// alert ("circle area is" + " " + area);
+
+// let distance = prompt ("what is the distance between cities in km?");
+// let time = prompt ("how long would you like to get? (in hour)");
+// let speed = distance / time;
+// alert ("you must move at a speed of" + " " + speed + " " + "km/hr");
+
+// let dollar = prompt ("enter the dollar amount");
+// const euro = dollar * 0.91;
+// alert ("it's" + " " + euro + " " + "euros");
+
+let flash = prompt ("specify the amount of flash drive in GB");
+const quantity = (flash / 820 * 1000);
+let answer = ("you can upload" + " " + quantity + " " + "files");
+alert (answer);
