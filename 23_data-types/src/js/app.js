1d68d48dd63e99f7b82906db15a9ffaf53032edb
tartalon <tartalon23@gmail.com>
2020-01-14 17:13:27 +0200

    23_js first

--- 23_data-types/src/js/app.js
+++ 23_data-types/src/js/app.js
@@ -8,61 +8,82 @@
 
 
 
-// let num = prompt ("Enter a number from 0 to 9");
-// switch (+num) {
-// 	case 0:
-// 		alert (")");
-// 		break;
-// 	case 1:
-// 		alert ("!");
-// 		break;
-// 	case 2:
-// 		alert ("@");
-// 		break;
-// 	case 3:
-// 		alert ("#");
-// 		break;
-// 	case 4:
-// 		alert ("$");
-// 		break;
-// 	case 5:
-// 		alert ("%");
-// 		break;
-// 	case 6:
-// 		alert ("^");
-// 		break;
-// 	case 7:
-// 		alert ("&");
-// 		break;
-// 	case 8:
-// 		alert ("*");
-// 		break;
-// 	case 9:
-// 		alert ("(");
-// 		break;
-// 	default:
-// 		alert ("Wrong number!");
-// }
+//=====First
 
-// First
-
-let age = document.querySelector('#i-age');
+let ageField = document.querySelector('#i-age');
 let check = document.querySelector('#submit-age');
 let result = document.querySelector('#title');
 
-function test() {
+check.addEventListener('click', function(e) {
 
-	if ((age <= 11) & (age > 0) & (age < 120)) {
+	e.preventDefault();
+ 	let age = +ageField.value;
+
+	if (age <= 11 && age > 0) {
 		result.textContent = 'Вы ребенок';
 	} else if (age <= 17) {
 		result.textContent = 'Вы подросток';
 	} else if (age <= 59) {
 		result.textContent ='Вы взрослый';
-	} else if (age >= 60) {
+	} else if (age >= 60 && age < 120) {
 		result.textContent = 'Вы пенсионер';
 	} else {
 		result.textContent = 'Вы, наверное, вели не верное число';
 	}
+});
+
+
+
+//=====Second
+
+let num = document.querySelector('#i-numb');
+let result2 = document.querySelector('#title');
+
+num.addEventListener('click')
+switch (+num) {
+	case 0:
+		alert (")");
+		break;
+	case 1:
+		alert ("!");
+		break;
+	case 2:
+		alert ("@");
+		break;
+	case 3:
+		alert ("#");
+		break;
+	case 4:
+		alert ("$");
+		break;
+	case 5:
+		alert ("%");
+		break;
+	case 6:
+		alert ("^");
+		break;
+	case 7:
+		alert ("&");
+		break;
+	case 8:
+		alert ("*");
+		break;
+	case 9:
+		alert ("(");
+		break;
+	default:
+		alert ("Wrong number!");
 }
 
-check.addEventListener('click', test);
+// let age = +ageField.value;
+
+// let test = (age <= 11) ? "Вы ребенок":
+// 	(age <= 17) ? "Вы подросток":
+// 	(age <= 59) ? "Вы взрослый":
+// 	(age > 59 && age < 120) ? "Вы пенсионер":
+// 	"Вы ввели неверный возраст";
+
+// check.addEventListener('click', test);
+// console.log(test);
+
+// result.textContent = test;
