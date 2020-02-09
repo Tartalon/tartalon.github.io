f7251498f1217c29205e4f6134497f52e5bd89a0
tartalon <tartalon23@gmail.com>
2020-02-09 23:24:23 +0200

    responsive menu done

--- graduation_project/src/js/app.js
+++ graduation_project/src/js/app.js
@@ -15,6 +15,13 @@ $('.description__img--box').slick({
 });
 
 
+// MENU=======
+document.querySelector('.header__nav--toggle').addEventListener('click', function toggle() {
+	document.querySelector('.nav__list').classList.toggle('nav__active');
+	document.querySelector('.header__nav--toggle').classList.toggle('toggle__active');
+});
+
+
   // infinite: true,
   // speed: 500,
   // fade: true,
