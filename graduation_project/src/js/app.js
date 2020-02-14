607c1b95c7985d78bd5529c083e32d3a49163ed3
Vladimir <tartalon23@gmail.com>
2020-02-14 18:40:53 +0200

    preloader

--- graduation_project/src/js/app.js
+++ graduation_project/src/js/app.js
@@ -70,3 +70,44 @@ modalClose.on("click", function(event) {
 	$("body").removeClass('no-scroll');
 });
 });
+
+
+
+
+// PRELOADER=========
+// let images = document.images,
+// 	images_total_count = images.length,
+// 	images_loaded_count = 0,
+// 	preloader = document.getElementById('page-preloader'),
+// 	perc_display = document.getElementById('load_perc');
+
+// for( let i = 0; i < images_total_count; i++ ) {
+// 	image_clone = new Image();
+// 	image_clone.onload = image_loaded;
+// 	image_clone.onerror = image_loaded;
+// 	image_clone.src = images[i].src;
+// }
+
+// function image_loaded() {
+// 	images_loaded_count++;
+
+// 	perc_display.innerHTML = (( (100 / images_total_count) * images_loaded_count ) << 0) + '%';
+
+// 	if( images_loaded_count >= images_total_count ){
+// 		setTimeout(function() {
+// 			let preloader = document.getElementById('page-preloader');
+// 			if( !preloader.classList.contains('done') ){
+// 				preloader.classList.add('done');
+// 			}
+// 		}, 1000);
+// 	}
+// }
+
+document.body.onload = function() {
+	setTimeout(function () {
+		let preloader = document.getElementById('page-preloader');
+		if(!preloader.classList.contains('done')) {
+			preloader .classList.add('done');
+		}
+	}, 2000);
+}
