8e66f3e0628bbf110fe91879d0d7664ae135a913
Vladimir <50200804+Tartalon@users.noreply.github.com>
2020-09-04 16:06:09 +0300

    fs flider done

--- movie/src/js/app.js
+++ movie/src/js/app.js
@@ -1,16 +1,20 @@
 // SWIPER==============
 var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
+  slidesPerView: 'auto',
   spaceBetween: 30,
   slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   clickable: true,
+  // },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+  autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
 });
