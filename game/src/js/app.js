48ee276efb924f1d5c13b8fe595261c3005607c4
Vladimir <50200804+Tartalon@users.noreply.github.com>
2022-01-06 16:21:49 +0200

    animate burger

--- game/src/js/app.js
+++ game/src/js/app.js
@@ -1,6 +1,8 @@
 const langInputWrapper = document.querySelector('.lang__input-wrapper');
 const langInput = document.querySelector('.lang__input');
 const langList = document.querySelector('.lang__list');
+const burger = document.querySelector('.header__burger');
+const navWrapper = document.querySelector('.nav__wrapper');
 
 langInputWrapper.addEventListener('click', e => {
 	langList.classList.toggle('show');
@@ -13,5 +15,16 @@ langList.addEventListener('click', e => {
 		e.target.textContent = selectedLang;
 
 		langList.classList.remove('show');
+	}
+});
+
+burger.addEventListener('click', e => {
+	if (!e.target.classList.contains('burger-active')) {
+		burger.classList.add('burger-active');
+		navWrapper.style.right = '-15px';
+	} else {
+		burger.classList.remove('burger-active');
+		navWrapper.style.right = '';
+		console.log(234);
 	}
 });
